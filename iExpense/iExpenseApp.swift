40bb9a50c88c4fa7bcd5c1f603984a43e2375896
alexliou <alexliou0@gmail.com>
2022-04-18 11:40:18 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alex Liou on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
